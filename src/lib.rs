a60d48660d58a2fe29d4767c9d6adb40e25ccdb7
WooSungD <woosung@float.capital>
2023-10-17 17:31:02 +0100

    add mod.rs for abigen and fix handlers

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
+mod abi;
 mod pb;
+extern crate lazy_static;
 
 use pb::example::{Contract, Contracts};
 use pb::example::{Swap, Swaps};
 
-use hex_literal::hex;
 use substreams::Hex;
 use substreams_entity_change::pb::entity::EntityChanges;
 use substreams_entity_change::tables::Tables;
 use substreams_ethereum::pb::eth;
 
-// const TRACKED_CONTRACT: [u8; 20] = hex!("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640");
+// Contract of the uniswap v3 swap pool converted into a hex array
+const TRACKED_CONTRACT: [u8; 20] = [
+    0x88, 0xe6, 0xA0, 0xc2, 0xdD, 0x26, 0xFE, 0xEb, 0x64, 0xF0, 0x39, 0xa2, 0xc4, 0x12, 0x96, 0xFc,
+    0xB3, 0xf5, 0x64, 0x0,
+];
 
 #[substreams::handlers::map]
 fn map_contract(block: eth::v2::Block) -> Result<Contracts, substreams::errors::Error> {
@@ -28,41 +33,49 @@ fn map_contract(block: eth::v2::Block) -> Result<Contracts, substreams::errors::
     Ok(Contracts { contracts })
 }
 
-// #[substreams::handlers::map]
-// fn map_swap(block: eth::v2::Block) -> Result<Swaps, substreams::errors::Error> {
-//     let swaps = block
-//     .events::<abi::erc721::events::Transfer>
-//         .calls()
-//         .filter(|view| !view.call.state_reverted)
-//         .filter(|view| view.call.call_type == eth::v2::CallType::Create as i32)
-//         .map(|view| Swap {
-//             id: format!("0x{}", Hex(&view.call.address)),
-//             sender: block.number,
-//             recipient:,
-//             amount0:,
-//             amount1:,
-//             sqrtPriceX96:,
-//             liquidity:,
-//             tick:,
-//             blockNumber:block.number,
-//             blockTimestamp: block.timestamp_seconds().to_string(),
-//             transactionHash: view.call.begin_ordinal,
-//         })
-//         .collect();
+#[substreams::handlers::map]
+fn map_swap(block: eth::v2::Block) -> Result<Swaps, substreams::errors::Error> {
+    let swaps: Vec<Swap> = block
+        .events::<abi::SwapContract::events::Swap>(&[&TRACKED_CONTRACT])
+        .map(|(swap, log)| {
+            substreams::log::info!("Swap event seen");
+            Swap {
+                id: block.hash.get(0).unwrap().to_string(),
+                sender: swap.sender.get(0).unwrap().to_string(),
+                recipient: swap.recipient.get(0).unwrap().to_string(),
+                amount0: swap.amount0.to_i32() as i64,
+                amount1: swap.amount1.to_i32() as i64,
+                sqrt_price_x96: swap.sqrt_price_x96.to_u64(),
+                liquidity: swap.liquidity.to_u64(),
+                tick: swap.tick.to_i32() as i64,
+                block_number: block.number as i64,
+                block_timestamp: block.timestamp_seconds() as i64,
+                transaction_hash: block.hash.clone().get(0).unwrap().to_string(),
+            }
+        })
+        .collect();
 
-//     Ok(Swaps { swaps })
-// }
+    Ok(Swaps { swaps })
+}
 
 #[substreams::handlers::map]
-pub fn graph_out(contracts: Contracts) -> Result<EntityChanges, substreams::errors::Error> {
+pub fn graph_out(swaps: Swaps) -> Result<EntityChanges, substreams::errors::Error> {
     // hash map of name to a table
     let mut tables = Tables::new();
 
-    for contract in contracts.contracts.into_iter() {
+    for swap in swaps.swaps.into_iter() {
         tables
-            .create_row("Contract", contract.address)
-            .set("timestamp", contract.timestamp)
-            .set("blockNumber", contract.block_number);
+            .create_row("Swap", swap.id)
+            .set("sender", swap.sender)
+            .set("recipient", swap.recipient)
+            .set("amount0", swap.amount0)
+            .set("amount1", swap.amount1)
+            .set("sqrt_price_x96", swap.sqrt_price_x96)
+            .set("liquidity", swap.liquidity)
+            .set("tick", swap.tick)
+            .set("block_number", swap.block_number)
+            .set("block_timestamp", swap.block_timestamp)
+            .set("transaction_hash", swap.transaction_hash);
     }
 
     Ok(tables.to_entity_changes())
